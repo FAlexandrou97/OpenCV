965f02015f0c55ba6ead4161e7c73dd99f364530
FAlexandrou97 <falexandrou@uclan.ac.uk>
2019-10-24 13:07:56 +0300

    Added tolerance slider but needs work.

--- Python/Projects/Week6/chromaKey.py
+++ Python/Projects/Week6/chromaKey.py
@@ -1,4 +1,5 @@
 # https://medium.com/fnplus/blue-or-green-screen-effect-with-open-cv-chroma-keying-94d4a6ab2743
+# Todo: Convert to HSV to make use of the slider
 import cv2
 import numpy as np
 
@@ -32,11 +33,25 @@ cv2.imshow("new", final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-while(cap.isOpened()):
+
+def do_nothing(eventStuff):
+    pass
+
+print(type(color))
+
+cv2.namedWindow('Frame')
+cv2.createTrackbar("Tolerance Slider", "Frame", 0, 100, do_nothing)
+while cap.isOpened():
     ret, frame = cap.read()
     if ret:
+        tolerance = cv2.getTrackbarPos("Tolerance Slider", "Frame")
         # Turn all pixels of selected color to black
-        mask = cv2.inRange(frame, color, color)
+        colorMin = color.copy()
+        colorMax = color.copy()
+        colorMin[0] = colorMin[0] - tolerance
+        colorMax[0] = colorMax[0] + tolerance
+
+        mask = cv2.inRange(frame, colorMin, colorMax)
         masked_asteroid = np.copy(frame)
         masked_asteroid[mask != 0] = [0, 0, 0]
 
@@ -47,7 +62,6 @@ while(cap.isOpened()):
         final = masked_asteroid + mask_on_second_image
         cv2.imshow('Frame', final)
         cv2.waitKey(10)
-
     else:
         break
 
