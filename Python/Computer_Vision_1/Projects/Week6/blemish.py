9f586ef86c210f82dcf497dc6212c2b63266d403
FAlexandrou97 <falexandrou@uclan.ac.uk>
2020-04-14 12:36:19 +0300

    Added image for the application to work.

--- Python/Computer_Vision_1/Projects/Week6/blemish.py
+++ Python/Computer_Vision_1/Projects/Week6/blemish.py
@@ -13,13 +13,13 @@ def click(event, x, y, flags, param):
         sobelxy = cv2.Sobel(imgBlur, cv2.CV_32F, 1, 1)
         mask = 255 * np.ones(roi.shape, roi.dtype)
         img = cv2.seamlessClone(sobelxy, img, mask, clickLocation, cv2.NORMAL_CLONE)
-        cv2.imshow("blemish", img)
 
         # Used for testing in the beginning
         '''
-        img = cv2.circle(img, clickLocation, 2, (0,255,0), 2)
-        imgBlur = cv2.GaussianBlur(roi,(5,5),7)
+        img = cv2.circle(img, clickLocation, 2, (0, 255, 0), 2)
+        imgBlur = cv2.GaussianBlur(roi, (5, 5), 7)
         '''
+        cv2.imshow("blemish", img)
 
 
 img = cv2.imread("blemish.png")
